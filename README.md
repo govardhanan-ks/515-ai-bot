ca744a3379d76464785cf1743fcb2ed7a28df8c7
govardhanan-sky <govardhanan.kanniyapansriramulu@sky.uk>
2025-06-12 17:45:50 +0530

    updated the readme file with and with out AI flag

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# 515-ai-bot
+# Slack 515 Bot - Run Instructions
+
+This Slack bot posts 515 updates to Confluence.
+
+---
+
+## 🧠 Run with AI (default)
+
+This uses an AI model (like LM Studio/OpenAI) to generate a clean summary of the message before sending it to Confluence.
+
+```bash
+python main.py
+
+```
+## Run With out AI
+
+``` bash
+
+python main.py --no-ai
+
+```
+
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from faulthandler import is_enabled
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 import os
@@ -6,16 +7,16 @@ from scripts.utils import Utils
 from dotenv import load_dotenv
 
 from scripts.altertable import AlterTable
+import sys
 
 load_dotenv()
 
 class Main:
-    def __init__(self):
+    def __init__(self, ai_enabled=True):
         self.app = App(token=os.environ.get("SLACK_TOKEN"))
-
-
         self.utils_app = Utils()
         self.alter_app = AlterTable()
+        self.ai_enabled = ai_enabled  # store the flag
         self.register_listeners()
 
     def register_listeners(self):
@@ -29,8 +30,14 @@ class Main:
             username = user_info["user"]["profile"].get("display_name") or user_info["user"]["profile"].get("real_name")
             print(f"Enginer {username} sending the 515 update")
 
-            response = self.utils_app.generate_content(inp)
-            print(f"Sending {response} as 515 to confluence")
+            #response = self.utils_app.generate_content(inp)
+            if self.ai_enabled:
+                response = self.utils_app.generate_content(inp)
+                print(f"Sending {response} as 515 to confluence (AI mode)")
+            else:
+                
+                response = inp
+                print(f"Sending raw input to confluence (Manual mode)")
 
             data = {}
             data["username"] = username
@@ -46,7 +53,9 @@ class Main:
 
 
 if __name__== "__main__":
-    Main().start()
+    import sys
+    use_ai = "--no-ai" not in sys.argv
+    Main(ai_enabled=use_ai).start()
 
 
 
